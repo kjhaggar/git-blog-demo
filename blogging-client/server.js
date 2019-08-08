de0c3d3e817c17f2960ed67c462722676da46502
kjhaggar <kjhaggar@bestpeers.com>
2019-08-08 13:22:01 +0530

    Deploy to heroku

--- blogging-client/server.js
+++ blogging-client/server.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require('path');
 
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/blog-client/index.html'));
 });
 
 app.listen(process.env.PORT || 4000, function(){
